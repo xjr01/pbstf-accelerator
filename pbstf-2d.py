41518a8e49f718093fec15cf67b79080e082c34f
xjr01 <xjr01@pku.edu.cn>
2024-06-13 23:08:04 +0800

    添加 surface 所有信息的准备

--- pbstf-2d.py
+++ pbstf-2d.py
@@ -2,10 +2,11 @@ import taichi as ti
 import taichi.math as tm
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 import os
 import time
 
-ti.init(arch=ti.cuda)
+ti.init(arch=ti.cuda, short_circuit_operators=True)
 
 N = ti.field(dtype=int, shape=())
 mass = ti.field(dtype=float, shape=())
@@ -19,11 +20,16 @@ density_eps = 200.
 distance_eps = 40.
 
 Nmax, Cmax = 10000, 50000
+Nangle = 360
+unit_angle = 2. * tm.pi / Nangle
+illuminated_threshold = 1. / 6.
 positions = ti.field(dtype=tm.vec2, shape=Nmax)
 velocities = ti.field(dtype=tm.vec2, shape=Nmax)
 densities = ti.field(dtype=float, shape=Nmax)
-on_surface = ti.field(dtype=ti.i8, shape=Nmax)
 normals = ti.field(dtype=tm.vec2, shape=Nmax)
+blocked = ti.field(dtype=int, shape=(Nmax, Nangle + 1))
+on_surface = ti.field(dtype=ti.i8, shape=Nmax)
+local_mesh_neighbors = ti.field(dtype=int, shape=(Nmax, 2))
 delta_positions = ti.field(dtype=tm.vec2, shape=Nmax)
 
 grid_size = [int(np.ceil(np.sqrt(Nmax))) * 5, int(np.ceil(np.sqrt(Nmax))) * 5]
@@ -130,6 +136,69 @@ def get_densities():
 					densities[i] += mass[None] * cubic_spline_kernel(pij_len, kernel_radius[None])
 
 @ti.kernel
+def get_surface_normal():
+	blocked.fill(0)
+	for i in range(N[None]):
+		normals[i] = tm.vec2(0, 0)
+		p = positions[i]
+		idx, idy = int((p[0] - x_min) // kernel_radius[None]), int((p[1] - y_min) // kernel_radius[None])
+		for gi in range(max(0, idx - 1), min(grid_size[0] - 1, idx + 1) + 1):
+			for gj in range(max(0, idy - 1), min(grid_size[1] - 1, idy + 1) + 1):
+				for j_id in range(grid_cnt[gi, gj]):
+					j = sorted_id[grid_offset[gi, gj] + j_id]
+					pj = positions[j]
+					pij = pj - p
+					pij_len = tm.length(pij)
+					if pij_len > kernel_radius[None] or i == j:
+						continue
+					normals[i] += mass[None] / densities[i] * cubic_spline_derivative(pij_len, kernel_radius[None]) * pij / pij_len
+					block_radius = ti.min(particle_radius[None], pij_len * .5)
+					angle = tm.atan2(pij[1], pij[0])
+					dangle = tm.asin(block_radius / pij_len)
+					st_angle = angle - dangle + (tm.pi * 2. if angle - dangle < -tm.pi else 0.)
+					en_angle = angle + dangle - (tm.pi * 2. if angle + dangle > tm.pi else 0.)
+					st = int((st_angle + tm.pi) // unit_angle)
+					en = int(tm.ceil((en_angle + tm.pi) / unit_angle))
+					if st < en:
+						blocked[i, st] += 1
+						blocked[i, en] -= 1
+					else:
+						blocked[i, st] += 1
+						blocked[i, Nangle] -= 1
+						blocked[i, 0] += 1
+						blocked[i, en] -= 1
+		illuminated_cnt = 0
+		for j in range(Nangle):
+			if j > 0:
+				blocked[i, j] += blocked[i, j - 1]
+			if blocked[i, j] == 0:
+				illuminated_cnt += 1
+		on_surface[i] = ti.i8(1 if illuminated_cnt >= illuminated_threshold * Nangle else 0)
+
+@ti.kernel
+def get_local_mesh():
+	local_mesh_neighbors.fill(-1)
+	for i in range(N[None]):
+		if on_surface[i] == 0:
+			continue
+		tangent = tm.normalize(tm.vec2(normals[i][1], -normals[i][0]))
+		p = positions[i]
+		idx, idy = int((p[0] - x_min) // kernel_radius[None]), int((p[1] - y_min) // kernel_radius[None])
+		for gi in range(max(0, idx - 1), min(grid_size[0] - 1, idx + 1) + 1):
+			for gj in range(max(0, idy - 1), min(grid_size[1] - 1, idy + 1) + 1):
+				for j_id in range(grid_cnt[gi, gj]):
+					j = sorted_id[grid_offset[gi, gj] + j_id]
+					pj = positions[j]
+					pij = pj - p
+					pij_len = tm.length(pij)
+					if pij_len > kernel_radius[None] or i == j or on_surface[j] == 0 or tm.dot(normals[i], normals[j]) <= 0:
+						continue
+					if tm.dot(pij, tangent) < 0. and (local_mesh_neighbors[i, 0] == -1 or tm.dot(positions[local_mesh_neighbors[i, 0]] - p, tangent) < tm.dot(pij, tangent)):
+						local_mesh_neighbors[i, 0] = j
+					if tm.dot(pij, tangent) > 0. and (local_mesh_neighbors[i, 1] == -1 or tm.dot(positions[local_mesh_neighbors[i, 1]] - p, tangent) > tm.dot(pij, tangent)):
+						local_mesh_neighbors[i, 1] = j
+
+@ti.kernel
 def apply_density_constraints(epsilon: float):
 	for i in range(N[None]):
 		p = positions[i]
@@ -201,9 +270,33 @@ def get_np_positions(np_positions: ti.types.ndarray()):
 		np_positions[i, 0] = positions[i][0]
 		np_positions[i, 1] = positions[i][1]
 
-def show_particles(p, save_file=None):
+@ti.kernel
+def get_np_surface_data(np_normals: ti.types.ndarray(), np_on_surface: ti.types.ndarray(), np_local_mesh: ti.types.ndarray()):
+	for i in range(N[None]):
+		np_normals[i, 0] = normals[i][0]
+		np_normals[i, 1] = normals[i][1]
+		np_on_surface[i] = on_surface[i]
+		np_local_mesh[i, 0] = local_mesh_neighbors[i, 0]
+		np_local_mesh[i, 1] = local_mesh_neighbors[i, 1]
+
+def show_particles(p, n=None, on_surf=None, local_mesh=None, save_file=None):
 	fig, ax = plt.subplots(figsize=(5, 5))
-	ax.scatter(p[:, 0], p[:, 1], s=1.)
+	ax.scatter(p[:, 0], p[:, 1], s=1., c=on_surf.astype(float), cmap='jet')
+	if n is not None:
+		normal_scale = ((n ** 2).sum(axis=-1) ** .5).max()
+		n = n[on_surf.astype(bool)] * kernel_radius[None] * 2. / normal_scale
+		drawn_normals = np.stack([p[on_surf.astype(bool)], p[on_surf.astype(bool)] + n], axis=1)
+		lc = LineCollection(drawn_normals, color='blue', linewidth=.5)
+		ax.add_collection(lc)
+	if local_mesh is not None:
+		has_meshes_0 = local_mesh[:, 0] != -1
+		drawn_meshes_0 = np.stack([p[has_meshes_0, :], p[local_mesh[has_meshes_0, 0], :]], axis=1)
+		has_meshes_1 = local_mesh[:, 1] != -1
+		drawn_meshes_1 = np.stack([p[has_meshes_1, :], p[local_mesh[has_meshes_1, 1], :]], axis=1)
+		lc0 = LineCollection(drawn_meshes_0, color='green', linewidth=.5)
+		lc1 = LineCollection(drawn_meshes_1, color='green', linewidth=.5)
+		ax.add_collection(lc0)
+		ax.add_collection(lc1)
 	ax.set_aspect('equal', adjustable='datalim')
 	ax.set_xlim(x_min, x_max)
 	ax.set_ylim(y_min, y_max)
@@ -217,31 +310,35 @@ def show_particles(p, save_file=None):
 
 if __name__ == '__main__':
 	init_square_droplet(-3., 3., -3., 3., 50)
+	print('particle number:', N[None])
 	init_neighbor_searcher()
 	mass[None] = 1.
 	get_densities()
 	mass[None] /= densities.to_numpy().max()
-	print('particle number:', N[None])
 	
-	st_time = time.time()
-	os.makedirs('output', exist_ok=True)
+	# st_time = time.time()
+	# os.makedirs('output', exist_ok=True)
+	# for iter in range(40):
+	# 	get_densities()
+	# 	get_surface_normal()
+	# 	constraints = densities.to_numpy()[:N[None]] / rest_density - 1.
+	# 	apply_density_constraints(density_eps)
+	# 	distance_constriant = apply_distance_constraints(distance_eps)
+	# 	print(f'constraint: {(constraints ** 2).sum() + distance_constriant}, time: {time.time() - st_time}')
+	# 	st_time = time.time()
+	# 	update_positions()
+	# 	init_neighbor_searcher()
+	get_densities()
+	get_surface_normal()
+	get_local_mesh()
+	# constraints = densities.to_numpy()[:N[None]] / rest_density - 1.
+	# distance_constriant = apply_distance_constraints(distance_eps)
+	# print(f'constraint: {(constraints ** 2).sum() + distance_constriant}, time: {time.time() - st_time}')
+	
 	vis_p = np.zeros((N[None], 2))
 	get_np_positions(vis_p)
-	show_particles(vis_p, f'output/particles_0.png')
-	print(f'Iteration 0 saved.')
-	for iter in range(40):
-		get_densities()
-		constraints = densities.to_numpy()[:N[None]] / rest_density - 1.
-		apply_density_constraints(density_eps)
-		distance_constriant = apply_distance_constraints(distance_eps)
-		print(f'constraint: {(constraints ** 2).sum() + distance_constriant}, time: {time.time() - st_time}')
-		st_time = time.time()
-		update_positions()
-		init_neighbor_searcher()
-	get_densities()
-	constraints = densities.to_numpy()[:N[None]] / rest_density - 1.
-	distance_constriant = apply_distance_constraints(distance_eps)
-	print(f'constraint: {(constraints ** 2).sum() + distance_constriant}, time: {time.time() - st_time}')
-	
-	get_np_positions(vis_p)
-	show_particles(vis_p, f'output/particles.png')
+	normal_p = np.zeros((N[None], 2))
+	on_surf = np.zeros(N[None], dtype=np.int8)
+	local_mesh = np.zeros((N[None], 2), dtype=np.int32)
+	get_np_surface_data(normal_p, on_surf, local_mesh)
+	show_particles(vis_p, n=normal_p, on_surf=on_surf, local_mesh=local_mesh, save_file=f'output/particles.png')
